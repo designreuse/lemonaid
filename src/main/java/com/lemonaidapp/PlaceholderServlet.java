c732374623bd7be21bfc0da2594ee9bda00321b1
ddcdanter <dante.rossi@dealer.com>
2014-07-16 10:14:26 -0400

    adding project euler problem #1 java file and updated Placeholder servlet to consume it

--- src/main/java/com/lemonaidapp/PlaceholderServlet.java
+++ src/main/java/com/lemonaidapp/PlaceholderServlet.java
@@ -15,9 +15,10 @@ public class PlaceholderServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // find some way to do the business logic here
         // ...
+        SumMultiplesOf3And5 number = new SumMultiplesOf3And5();
 
         // put the required output in the request attributes to share with the jsp
-        req.setAttribute("placeholder", "This is a placeholder!");
+        req.setAttribute("placeholder", number);
         getServletContext().getRequestDispatcher("/Placeholder.jsp").forward(req, resp);
     }
 
---
+++ src/main/java/com/lemonaidapp/SumMultiplesOf3And5.java
@@ -0,0 +1,22 @@
+package com.lemonaidapp;
+
+/**
+ * Created by ddcdanter on 7/15/14.
+ */
+public class SumMultiplesOf3And5 {
+
+        public int sum(int input){
+            int sum=0;
+            for(int i=0; i<input; i++) {
+                if((i%3 == 0) || (i%5 == 0)){
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public static void main(String[] args){
+            SumMultiplesOf3And5 number = new SumMultiplesOf3And5();
+            System.out.println(number.sum(1000));
+        }
+    }
