32be2bdd6951faa339eed5fc6458b09a898d5cbf
ddcdanter <dante.rossi@dealer.com>
2014-08-01 17:12:28 -0400

    changed servlets that handle doGet and doPost to get the servlet to display again

--- src/main/java/com/lemonaidapp/maintenance/PostServlet.java
+++ src/main/java/com/lemonaidapp/maintenance/PostServlet.java
@@ -17,28 +17,6 @@ public class PostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<MaintenanceEvent> events = new ArrayList<MaintenanceEvent>();
-
-        MaintenanceEvent oilChange = new MaintenanceEvent();
-        Calendar oilChangeCal = Calendar.getInstance();
-        oilChangeCal.set(2014, 4, 15);
-        oilChange.setDate(oilChangeCal.getTime());
-        oilChange.setTask("Changed Oil");
-        oilChange.setComments("4 Quarts Mobil1");
-        oilChange.setVehicleName("Subaru Impreza");
-        events.add(oilChange);
-
-        MaintenanceEvent brakeWork = new MaintenanceEvent();
-        Calendar brakeWorkCal = Calendar.getInstance();
-        brakeWorkCal.set(2014, 4, 15);
-        brakeWork.setDate(brakeWorkCal.getTime());
-        brakeWork.setTask("Changed Brake Pads");
-        brakeWork.setComments("Life expectancy: 60,000 miles");
-        brakeWork.setVehicleName("Subaru Impreza");
-        events.add(brakeWork);
-
-        req.setAttribute("events", events);
-        getServletContext().getRequestDispatcher("/maintenance/listing.jsp").forward(req, resp);
-
+        doGet(req,resp);
     }
 }
