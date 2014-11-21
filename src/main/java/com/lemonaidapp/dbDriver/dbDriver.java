a727f7176de2dbf95df4715e5b020a27ee68d92b
ddcdanter <dante.rossi@dealer.com>
2014-11-21 15:09:04 -0500

    sql driver start

---
+++ src/main/java/com/lemonaidapp/dbDriver/dbDriver.java
@@ -0,0 +1,48 @@
+package com.lemonaidapp.dbDriver;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by ddcdanter on 11/18/14.
+ */
+public class dbDriver {
+
+    public static void main(String[] args) throws SQLException {
+        String URL = "jdbc:mysql://localhost:3306/lemonaid";
+        //String username = "username";
+        //String password = "password";
+
+        try {
+            Connection con = DriverManager.getConnection(URL);
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM MaintenanceEventRepo");
+
+            while (rs.next()) {
+                String vehicleName = rs.getString("VehicleName");
+                String task = rs.getString("Task");
+                String comments = rs.getString("Comments");
+                int mileage = rs.getInt("Mileage");
+                int id = rs.getInt("Id");
+                Date date = rs.getDate("Date");
+
+                System.out.println(vehicleName + "\n"
+                        + task + "\n"
+                        + comments + "\n"
+                        + mileage + "\n"
+                        + id + "\n"
+                        + date + "\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } //finally {
+            //try {
+                //if (){}
+            //} catch (SQLException e){
+                //e.printStackTrace();
+            //}
+        }
+}
