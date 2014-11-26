b33c81dd820a0aa352627b1b5eb44e62364adba6
ddcdanter <dante.rossi@dealer.com>
2014-11-26 11:05:55 -0500

    update schema, added primary key and second entry

--- schema/lemonaid.sql
+++ schema/lemonaid.sql
@@ -6,7 +6,7 @@ VehicleName varchar(255),
 Task varchar(255),
 Comments varchar(255),
 Mileage int,
-Id int,
+Id int PRIMARY KEY,
 Date date
 );
 
@@ -26,4 +26,22 @@ values
 21345,
 019283756,
 "This is a test"
+);
+
+insert into maintenance_events
+(
+VehicleName,
+Task,
+Mileage,
+Id,
+Comments
+)
+
+values
+(
+"My Ford",
+"Tire change",
+100101,
+5647382910,
+"This is a second test"
 );
